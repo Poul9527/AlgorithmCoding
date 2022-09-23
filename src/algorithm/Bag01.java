3c2249ac0bf4b2badcd44056e2663b49d364e2b6
poul9527 <505262148@qq.com>
2022-09-23 09:04:45 +0800

    01

--- src/algorithm/Bag01.java
+++ src/algorithm/Bag01.java
@@ -1,21 +1,19 @@
-package algorithm;
-
 /**
- * n����Ʒ��1��������ÿ����Ʒi��Ӧ������Ϊwi����ֵΪvi������������ΪW��
- * ÿ����Ʒֻ��һ����Ҫôװ�룬Ҫô��װ�룬���ɲ�֡�
- * ���ѡȡ��Ʒװ�뱳����ʹ������װ�����Ʒ���ܼ�ֵ���
+ * n个物品和1个背包，每个物品i对应的重量为wi，价值为vi，背包的容量为W。
+ * 每个物品只有一件，要么装入，要么不装入，不可拆分。
+ * 如何选取物品装入背包，使背包所装入的物品的总价值最大？
  */
 
 public class Bag01 {
 
     public void wei_bag_problem(int[] weight, int[] value, int w) {
 
-        // ��ʼ��
+        // 初始化
         int[] dp = new int[w + 1];
         dp[0] = 0;
-        // ������Ʒ
+        // 遍历物品
         for (int i = 0; i < weight.length; i++) {
-            // ������������
+            // 遍历背包容量
             for (int j = w; j >= weight[i]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
             }
